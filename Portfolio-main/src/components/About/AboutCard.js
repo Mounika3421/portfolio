c2b8e9e3113cfa1711153bd48c7429bed6b7d1c6
Mounika3421 <157018984+Mounika3421@users.noreply.github.com>
2024-09-01 12:14:56 +0530

    Add files via upload

---
+++ Portfolio-main/src/components/About/AboutCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Sanjay Reddy Muthyala </span>
+            from <span className="purple"> Hyderabad, India.</span>
+            <br />
+            I am currently pursuing B.Tech 4th year at Chaitanya Bharathi Institute of Technology.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Photography
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Sanjay Reddy</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
